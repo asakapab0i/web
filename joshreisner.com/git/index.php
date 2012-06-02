795cad2f6a217a36851d34af513e03e065403fac
Josh Reisner <josh@joshreisner.com>
2012-06-02 12:08:40 -0400

    more

--- joshreisner.com/git/index.php
+++ joshreisner.com/git/index.php
@@ -4,18 +4,7 @@ include('include.php');
 if (!empty($_POST['payload']) && in_array($_SERVER['REMOTE_ADDR'], array('207.97.227.253', '50.57.128.197', '108.171.174.178'))) {
 	//incoming data is from valid github server
 	
-	$data = $_POST['payload']; 
-	
-	$subject = count($data['commits']) . " new commits";
-/*
-	$body = "New code has been committed to the project: \n"; 
-	
-	foreach($data['commit'] as $commit) { 
-		$body .= "Commit: " . $commit['id'] . "\n"; 
-		$body .= $commit['message'] . "\n\n"; 
-	} 
-*/ 
-	
+	$subject = 'New Commit Report';
 	$body = draw_array(json_decode($_POST['payload']));
 		
 	email('josh@joshreisner.com', $body, $subject);
