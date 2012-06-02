21bcfa880beb155652f260f52990f719710df1eb
Josh Reisner <josh@joshreisner.com>
2012-06-02 12:13:37 -0400

    working on git deploy thing more

--- joshreisner.com/git/index.php
+++ joshreisner.com/git/index.php
@@ -1,5 +1,7 @@
 <?php
+$_josh['mode'] = 'dev';
 include('include.php');
+error_reporting(E_ALL);
 
 if (!empty($_POST['payload']) && in_array($_SERVER['REMOTE_ADDR'], array('207.97.227.253', '50.57.128.197', '108.171.174.178'))) {
 	//incoming data is from valid github server
